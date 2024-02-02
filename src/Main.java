d1123d418c2b53578baca118976ba25dc966c9eb
lymann <phplaravel576@gmail.com>
2024-02-02 10:18:29 +0700

    team members

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Mengseu");
+        System.out.println("Sokny");
+        System.out.println("Thyda");
+        System.out.println("Taing Ey");
+        System.out.println("Vipha");
+        System.out.println("Project Leader");
     }
 }
